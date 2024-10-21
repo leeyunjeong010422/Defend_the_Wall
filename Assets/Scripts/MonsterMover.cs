277700ef2ae982774ff8bdb4048d0caeab241735
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-10-21 11:23:39 +0900

    feat: 몬스터 Nav 구현

--- Assets/Scripts/MonsterMover.cs
+++ Assets/Scripts/MonsterMover.cs
@@ -1,14 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterMover : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] float locomotionSpeed;
 
+    [SerializeField] GameObject monsterAttackPoint;
+
+    [SerializeField] Transform target;
+
+    NavMeshAgent navMeshAgent;
+
     private void Start()
     {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
         animator.SetFloat("Locomotion", 1);
+    }
+
+    private void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == monsterAttackPoint)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("Attack2");
     }
 }
